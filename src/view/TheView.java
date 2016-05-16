02592f54f16319783ac9b07b16784e4a1370a9ac
drugzy <looy__24@hotmail.com>
2016-05-16 16:46:44 +0200

    Added Menu

--- src/view/TheView.java
+++ src/view/TheView.java
@@ -20,6 +20,7 @@ import model.ModelTexture;
 import model.OBJLoader;
 import model.ModelInfo;
 import model.ModelAndTexuredInfo;
+import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -28,51 +29,48 @@ import org.lwjgl.util.vector.Vector3f;
  * @author yehia
  */
 public class TheView {
-    
-    
-    public void view() throws IOException{
-        DisplayManager.createDisplay();
-		DataLoaderVAO loader = new DataLoaderVAO();
-		
-		
-		ModelInfo model = OBJLoader.loadObjModel("laastsphere", loader);
-		
-		ModelAndTexuredInfo staticModel = new ModelAndTexuredInfo(model,new ModelTexture(loader.loadTexture("ball","jpg")));
-		
-		List<Entity> entities = new ArrayList<Entity>();
-		Random random = new Random();
-                
-                Player myPlayer = new Player(staticModel, new Vector3f(0,5,0),0,180,0,2.5f);
-                
-		
-		Light light = new Light(new Vector3f(350,2000,200),new Vector3f(1,1,1));
-		
-		
-                Terrain terrain = new Terrain(0.5f,0,loader,new ModelTexture(loader.loadTexture("grass","PNG")));
-		Terrain terrain2 = new Terrain(0.5f,-1.5f,loader,new ModelTexture(loader.loadTexture("grass","PNG")));
-                		
-		Camera camera = new Camera(myPlayer);	
-		MasterRenderer renderer = new MasterRenderer();
-		
-		while(!Display.isCloseRequested()){
-	
-                       
-			renderer.processTerrain(terrain);
-			renderer.processTerrain(terrain2);
-			
-                        renderer.processEntity(myPlayer);
-                        
-                        camera.move();
-                        
-                        myPlayer.move();
-                       
-			renderer.render(light, camera);
-			DisplayManager.updateDisplay();
-		}
 
-		renderer.cleanUp();
-		loader.cleanUP();
-		DisplayManager.closeDisplay();
+    public static void view() throws IOException, LWJGLException {
+       // DisplayManager.createDisplay();
+        DataLoaderVAO loader = new DataLoaderVAO();
 
-      }
+        ModelInfo model = OBJLoader.loadObjModel("laastsphere", loader);
+
+        ModelAndTexuredInfo staticModel = new ModelAndTexuredInfo(model, new ModelTexture(loader.loadTexture("ball", "jpg")));
+
+        List<Entity> entities = new ArrayList<Entity>();
+        Random random = new Random();
+
+        Player myPlayer = new Player(staticModel, new Vector3f(0, 5, 0), 0, 180, 0, 2.5f);
+
+        Light light = new Light(new Vector3f(350, 2000, 200), new Vector3f(1, 1, 1));
+
+        Terrain terrain = new Terrain(0.5f, 0, loader, new ModelTexture(loader.loadTexture("grass", "PNG")));
+        Terrain terrain2 = new Terrain(0.5f, -1.5f, loader, new ModelTexture(loader.loadTexture("grass", "PNG")));
+
+        Camera camera = new Camera(myPlayer);
+        MasterRenderer renderer = new MasterRenderer();
+
+        while (!Display.isCloseRequested()) {
+
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+
+            renderer.processEntity(myPlayer);
+
+            camera.move();
+
+            myPlayer.move();
+
+            renderer.render(light, camera);
+           /* Menu menu= new Menu();
+            menu.writeFont("ssss");*/
+            DisplayManager.updateDisplay();
+        }
+
+        renderer.cleanUp();
+        loader.cleanUP();
+        DisplayManager.closeDisplay();
+
+    }
 }
